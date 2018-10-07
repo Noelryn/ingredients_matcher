099cbe00c0d3ff9c68e99ba19f1f5511b58976be
Noel Ryan <noelgerard.ryan@gmail.com>
2018-10-07 22:39:58 +0200

    added in search function currently failing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,10 @@
 class RecipesController < ApplicationController
     def index
-        @recipes = RecipesService.get_recipes[1]
+        @recipes = RecipesService.search(params[:search])
+    end
+
+    private
+    def recipes_params
+        params.require(:recipe).permit(:name, :title, :search)
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
+    belongs_to :recipes
     validates :name, presence: true
+
+    def self.search(search)
+        if search
+            recipe = Recipe.find_by(name: search)
+            if recipe
+                self.where(title: recipe)
+            else
+                Recipes.all
+            end
+        else
+            Recipes.all
+        end
+    end
 end
